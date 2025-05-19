5510bca08c7ef8b28d922ae780d23e680040c8e3
Nezha El Fayez <nezhaelfayez@gmail.com>
2025-05-19 15:30:53 +0200

    creation composants

---
+++ FrontErua/src/app/type-selector-component/type-selector-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-type-selector-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './type-selector-component.component.html',
+  styleUrl: './type-selector-component.component.scss'
+})
+export class TypeSelectorComponentComponent {
+
+}
