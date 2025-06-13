cb2869c17fd70b445a380366f124c874500654cd
Nezha El Fayez <nezhaelfayez@gmail.com>
2025-06-13 19:53:16 +0200

    ajout footer ;)

--- FrontErua/src/app/app.component.ts
+++ FrontErua/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Sidebarcomponent } from './components/sidebar/sidebarcomponent';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer-component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, Sidebarcomponent, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, Sidebarcomponent, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
